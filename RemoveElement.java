b120fcc250f62768f2ebee57827808c1e29cc8e3
haigouhhhh <775065739@qq.com>
2017-09-25 23:28:54 -0400

    RemoveElement.java

---
+++ RemoveElement.java
@@ -0,0 +1,20 @@
+package helloworld;
+
+public class RemoveElement
+{
+	public int removeElement(int[] nums, int val)
+    {
+        if(nums.length==0)
+            return 0;
+        int i=0;
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[j]!=val)
+            {
+                nums[i]=nums[j];
+                i=i+1;
+            }
+        }
+        return i;
+    }
+}
